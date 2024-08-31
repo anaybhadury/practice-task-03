64519a567acbd7bd6e1f5874678dcffa97941748
anaybhadury <bhadury9292@gmail.com>
2024-08-31 16:33:17 +0600

    added practice task 3

---
+++ task-3.js
@@ -0,0 +1,19 @@
+/**
+ * -------Practice Task--------
+ * Create an array of books containing different book.
+ * Use the includes method to check if the array contains a javascript book.
+ * Print a message to the console indicating whether the element is present in the array or not.
+ */
+
+const books = ['bangla', 'english', 'mathmatics', 'science', 'art'];
+
+// console.log(books.includes('art')); true
+
+console.log(books.includes('javascript')); false
+
+
+
+
+
+
+
