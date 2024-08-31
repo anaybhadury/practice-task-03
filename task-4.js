4ef5d5149e26d86a439ab71359be4ea119f29944
anaybhadury <bhadury9292@gmail.com>
2024-08-31 16:52:51 +0600

    added practice task 4

---
+++ task-4.js
@@ -0,0 +1,26 @@
+/**
+ * -------Practice Task--------
+ * Create different variables, each containing either an array or a non-array value.
+ * Now use isArray to check if each variable is an array.
+ * Print a message to the console indicating whether each variable is an array or not.
+ */
+
+const ages = 49;
+const fruits = ['mango', 'orange', 'jambura'];
+const arrayCheck = Array.isArray(fruits)
+// console.log(Array.isArray(ages));
+// console.log(Array.isArray(fruits));
+
+if(arrayCheck === true){
+    console.log('this is array')
+}
+else{
+    console.log('this is not array')
+}
+
+
+
+
+
+
+
