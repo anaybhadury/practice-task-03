89b9a3ff64086214dbc93e748a6c9fbf9ff91ada
anaybhadury <bhadury9292@gmail.com>
2024-08-31 16:16:05 +0600

    added task 1

---
+++ task-1.js
@@ -0,0 +1,36 @@
+/**
+ * -------Practice Task--------
+ * Declare an array with 5 elements containing fruits
+ * console log the 3rd index element
+ * change the value of the 2nd index element to jambura
+ * console log the final array
+ */
+
+const fruits = ['banana','rosogolla','bel','anarosh','mango'];
+console.log(fruits[3]);
+
+fruits[2] = 'jambura';
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+// const fruits = ['apple','mango','banana','orange','cucamber'];
+
+// console.log(fruits[3]);
+
+// fruits[2] = 'jambura';
+
+// console.log(fruits);
+
+
+
+
