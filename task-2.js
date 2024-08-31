a978f1f16b84c1d1ee4930db243464b0e26b1e13
anaybhadury <bhadury9292@gmail.com>
2024-08-31 16:24:52 +0600

    added practice task 2

---
+++ task-2.js
@@ -0,0 +1,30 @@
+/**
+ * -------Practice Task--------
+ * Declare an array of 3 tourist destinations
+ * Add a new tourist destination to your tourist array
+ * Add two more to your array
+ * Remove the last tourist destination you have added
+ * display the final array as output
+ */
+
+const destinations = ['sea-beatch', 'kaptay', 'bandorban'];
+
+destinations.push('rangamati');
+
+// console.log(destinations);
+
+// destinations.push('thanchi', 'sylhet jaflong');
+
+destinations.push('thanchi');
+destinations.push('sylhet jaflong');
+
+// console.log(destinations);
+
+destinations.pop();
+console.log(destinations)
+
+
+
+
+
+
